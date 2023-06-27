7c7af454a29ae6ba8b06be39417a4406e51bcdee
dawoyo <111173354+dawoyo@users.noreply.github.com>
2023-06-26 20:48:21 -0600

    Create common.groovy

---
+++ vars/common.groovy
@@ -0,0 +1,12 @@
+def call(string stageName){
+  if("${stageName}" == "Build"){
+    sh "mvn package"
+<!--  }
+  else if("${stageName}" == "Test"){
+    sh "mvn sonar:sonar"
+    -->
+  }
+  else if("${stageName}" == "Deploy"){
+    sh "mvn deploy"
+  }
+}
